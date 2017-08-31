d59350a2c04c36ced5f4a82da938ceee62028610
raklan <rcl.catcher@gmail.com>
2017-08-31 14:28:18 -0600

    Aug31

---
+++ August31/SQRT.java
@@ -0,0 +1,12 @@
+public class SQRT {
+
+    public static void main(String[]args){
+
+            int a = 3;
+            int b = 4;
+            int c = (int)Math.sqrt(a*a + b*b);
+
+            System.out.println("The Hypotenuse is "+c);
+    }
+
+}
