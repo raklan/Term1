ad3452443234facd85be87bb61a650b767806ca4
raklan <rcl.catcher@gmail.com>
2017-11-01 13:59:27 -0600

    Circle Loops

---
+++ Nov 1/LoopingCircles/src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame win = new JFrame("Boopity Bopity");
+        win.setLayout(null);
+        win.setBounds(50,50,1000,1000);
+        win.setVisible(true);
+        win.setDefaultCloseOperation(win.EXIT_ON_CLOSE);
+
+        int num = 0;
+
+        int xPos;
+        int yPos;
+
+        new Oval(0,0,10,10);
+
+        while(num<=5){
+
+        }
+
+
+    }
+}
